c956a52f38ced56bea9eba73679d4b3d857b2995
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-04-24 14:06:45 -0700

    Modified start script, JS for answers object

--- index.js
+++ index.js
@@ -2,44 +2,55 @@
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-inquirer
-    .createPromptModule([
-        {
-            type: 'input',
-            message: 'What is the name of this project?',
-            name: 'project name',
-        },
-        {
-            type: 'input',
-            message: 'Description?',
-            name: 'description',
-        },
-        {
-            type: 'input',
-            message: 'What is the color of the sky?',
-            name: 'sky color',
-        },
-        {
-            type: 'input',
-            message: 'What is the meaning of life?',
-            name: 'meaning of life',
-        },
-        {
-            type: 'input',
-            message: 'How old are you?',
-            name: 'age',
-        },
-        {
-            type: 'input',
-            message: 'What are your plans for today?',
-            name: 'plans',
-        },
-        {
-            type: 'input',
-            message: 'How many colors are in the rainbow?',
-            name: 'colors',
-        },
-    ])
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the name of this project?',
+        name: 'project name',
+    },
+    {
+        type: 'input',
+        message: 'Description?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What is the color of the sky?',
+        name: 'sky color',
+    },
+    {
+        type: 'input',
+        message: 'What is the meaning of life?',
+        name: 'meaning of life',
+    },
+    {
+        type: 'input',
+        message: 'How old are you?',
+        name: 'age',
+    },
+    {
+        type: 'input',
+        message: 'What are your plans for today?',
+        name: 'plans',
+    },
+    {
+        type: 'input',
+        message: 'How many colors are in the rainbow?',
+        name: 'colors',
+    },
+]
+
+function generator() {
+    return inquirer.prompt(questions)
+    .then((data)=>{
+        console.log(data)
+        return data
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+}
+generator();
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
